4f2a8dd8ac480821a6cb51f1bf3ea1d494c32586
chenguang.zhang <chenguang.zhang@zhangmen.com>
2018-03-27 23:29:02 +0800

    init

--- config/dev.env.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-const merge = require('webpack-merge')
-const prodEnv = require('./prod.env')
-
-module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"',
-  BASE_API: '"http://partner-test.zmlearn.com:8081"',
-  HOST: '"http://partner-test.zmlearn.com:8081"'
-})
